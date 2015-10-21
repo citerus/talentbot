219c2f62c81b59afca0dc81fb5347a95eb48ce60
Rickard Sundin <rickard@snart.nu>
2015-10-21 16:53:01 +0200

    Add template for setting up keys in environment

---
+++ keys-template.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Save this file as keys.sh before entering your own keys.
+# The file should be ignored by your VCS and should not be committed.
+# Run "source keys.sh" to set keys in your environment.
+
+TRELLO_API_KEY=Your key here
+TRELLO_API_SECRET=Your key here
+TRELLO_TOKEN=Your key here
+TRELLO_TOKEN_SECRET=Your key here
+
+SLACK_TOKEN=Your key here
+
+export TRELLO_API_KEY TRELLO_API_SECRET TRELLO_TOKEN TRELLO_TOKEN_SECRET SLACK_TOKEN
+
