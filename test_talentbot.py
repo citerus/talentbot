a9c38939def01ed5faeb6bac9fffdd9719939093
Rickard Sundin <rickard@snart.nu>
2015-10-25 12:12:51 +0100

    Keyword test uses message events

--- test_talentbot.py
+++ test_talentbot.py
@@ -8,21 +8,16 @@ class SlackEventTest(unittest.TestCase):
         self.assertTrue(event.isMessage())
 
     def testCanIdentifySingleKeyword(self):
-        event = eventWithText("banana")
+        event = messageWithText("banana")
         self.assertTrue(event.textContainsKeyword("banana"))
 
     def testCanIdentifyKeywordFollowedByArgument(self):
-        event = eventWithText("banana split")
+        event = messageWithText("banana split")
         self.assertTrue(event.textContainsKeyword("banana"))
 
 def messageWithText(text):
     event = {}
     event['type'] = 'message'
-    event['text'] = text
-    return SlackEvent(json.loads(json.dumps(event)))
-
-def eventWithText(text):
-    event = {}
     event['text'] = text
     return SlackEvent(json.loads(json.dumps(event)))
 
