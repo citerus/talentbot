54de7e061ac244c9d7fa2302fedcf9c09edf96da
Rickard Sundin <rickard@snart.nu>
2015-10-25 12:18:38 +0100

    Test isTalentBot()

--- test_talentbot.py
+++ test_talentbot.py
@@ -1,11 +1,15 @@
 import unittest
 import json
-from talentbot import SlackEvent
+from talentbot import SlackEvent, TALENTBOT_USER_ID
 
 class SlackEventTest(unittest.TestCase):
     def testCanIdentifyMessage(self):
         event = messageWithText("hej")
         self.assertTrue(event.isMessage())
+
+    def testCanIdentifyMessageFromSelf(self):
+        event = messageFromUser(TALENTBOT_USER_ID)
+        self.assertTrue(event.isTalentBot())
 
     def testCanIdentifySingleKeyword(self):
         event = messageWithText("banana")
@@ -15,11 +19,17 @@ class SlackEventTest(unittest.TestCase):
         event = messageWithText("banana split")
         self.assertTrue(event.textContainsKeyword("banana"))
 
+def messageFromUser(user):
+    event = {}
+    event['type'] = 'message'
+    event['user'] = user
+    return SlackEvent(event)
+
 def messageWithText(text):
     event = {}
     event['type'] = 'message'
     event['text'] = text
-    return SlackEvent(json.loads(json.dumps(event)))
+    return SlackEvent(event)
 
 def main():
     unittest.main()
