a9d9d4908112c85237e47504a50693db4e438711
Tobias Fors <tobias.fors@citerus.se>
2015-10-27 20:50:26 +0100

    Removed debugging info.

--- command.py
+++ command.py
@@ -40,7 +40,7 @@ class FindPeopleByTalent(Command):
         if len(talent) > 0:
             person_emails = self.trello.getPersonEmailsByTalent2(talent)
             people = self.persons_by_emails(person_emails)
-            self.slack.rtm_send_message(event.channel(), "DEV Personer med talangen " + talent + ": " + people)
+            self.slack.rtm_send_message(event.channel(), "Personer med talangen " + talent + ": " + people)
         else:
             self.slack.rtm_send_message(event.channel(), "Talangen " + talent + " ej funnen")
             print "called for persons with a talent"
