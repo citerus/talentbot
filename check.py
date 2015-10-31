07d624e0558e08de19cf69cd6324f4a8698881f6
tofo <filanderknupp@server.fake>
2015-10-31 16:45:08 +0100

    Convenience script for checking dev environment.

---
+++ check.py
@@ -0,0 +1,35 @@
+import os
+import sys
+
+def check_key(key_name):
+    sys.stdout.write("Checking key %s ... " % key_name)
+    try:
+        key = os.environ[key_name]
+        print("OK")
+    except KeyError:
+        print "Missing!"
+        
+def check_virtual_env():
+    sys.stdout.write("Checking virtual env ...")
+    if hasattr(sys, 'real_prefix'):
+        print("OK")
+    else:
+        print("Not running in virtual environment!")
+        
+def check_python():
+    sys.stdout.write("Checking Python version ...")
+    if (sys.version_info > (2,7,10)):
+        print("OK ({0},{1},{2})".format(sys.version_info.major, sys.version_info.minor, sys.version_info.micro))
+    else:
+        print("Problematic.")
+        
+
+key_names = ["TRELLO_API_KEY", \
+    "TRELLO_API_SECRET", "TRELLO_TOKEN", \
+    "TRELLO_TOKEN_SECRET", "SLACK_TOKEN"]
+    
+for key_name in key_names:
+    check_key(key_name)
+    
+check_virtual_env()
+check_python()
