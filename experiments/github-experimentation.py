578fff3dbd7f3f499228dd263eb29897b832a2e4
orende <ola.rende@citerus.se>
2015-11-04 22:34:55 +0100

    WIP on getting languages from Github (mind the API rate limit)

--- experiments/github-experimentation.py
+++ experiments/github-experimentation.py
@@ -1,9 +1,21 @@
-import json
+import time
+import grequests
 
 #curl https://api.github.com/users/decker108/repos
 
-data = ''
-with open('repos.json', 'r') as myfile:
-    data = myfile.readlines()
-    
-#jsonData = json.loads(data)
+# response = grequests.get('https://api.github.com/users/decker108/repos')
+# responseJson = response.json()
+
+startTime = time.time()
+# languageUrls = [entry['languages_url'] for entry in response]
+# print languageUrls
+
+languageUrls = ['http://www.google.com', 'http://www.example.com', 'http://www.python.org']
+# jobs = [spawn(grequests.get, url) for url in urls]
+# joinall(jobs, timeout=2)
+# print [job.value for job in jobs]
+rs = [grequests.get(u) for u in languageUrls]
+print [r.status_code for r in (grequests.map(rs))]
+print 'endtime ', time.time()-startTime
+
+#TODO use OAuTH2 auth to increase rate limit
