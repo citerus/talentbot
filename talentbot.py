307e8d9c3a954bf7d5075fecac2dc3fadbce792c
Rickard Sundin <rickard@snart.nu>
2015-11-15 12:33:42 +0100

    Add newline for increased readability

--- talentbot.py
+++ talentbot.py
@@ -53,7 +53,7 @@ class TalentBot:
             logging.debug("Incoming message\n- %s" % event)
 
         if event.textContainsKeyword('help'):
-            self.slack.rtm_send_message(event.channel(), ":paperclip: It looks like you need help." + '\n'.join(cmd.help() for cmd in self.commands))
+            self.slack.rtm_send_message(event.channel(), ":paperclip: It looks like you need some help.\n" + '\n'.join(cmd.help() for cmd in self.commands))
             return
 
         for command in self.commands:
