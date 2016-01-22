7b7043015dc1a31bc6535304705231f7a100a004
Rickard Sundin <rickard@snart.nu>
2016-01-22 14:25:53 +0100

    Hello world in flask

---
+++ slash.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask
+
+# Set these variables in your local environment (export TRELLO_TOKEN=abcd)
+apiKey      = os.environ.get('TRELLO_API_KEY')
+apiSecret   = os.environ.get('TRELLO_API_SECRET')
+tr_token    = os.environ.get('TRELLO_TOKEN')
+tokenSecret = os.environ.get('TRELLO_TOKEN_SECRET')
+#token       = os.environ.get('SLACK_TOKEN')
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Awesome!"
+
+if __name__ == "__main__":
+    app.run()
+    
