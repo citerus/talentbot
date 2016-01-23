d176a896508400fbb9d8f23105cda488ce82c783
Rickard Sundin <rickard@snart.nu>
2016-01-23 08:55:40 +0100

    Add simple slack integration

--- slash.py
+++ slash.py
@@ -1,32 +1,51 @@
-import os
+import os, logging
 from flask import Flask, request
+from slackclient import SlackClient
+from slackuser import SlackUser
+
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger('fileHandler')
 
 # Set these variables in your local environment (export TRELLO_TOKEN=abcd)
 slash_token = os.environ.get('SLASH_TOKEN').strip()
+slack_token = os.environ.get('SLACK_TOKEN').strip()
 apiKey      = os.environ.get('TRELLO_API_KEY')
 apiSecret   = os.environ.get('TRELLO_API_SECRET')
 tr_token    = os.environ.get('TRELLO_TOKEN')
 tokenSecret = os.environ.get('TRELLO_TOKEN_SECRET')
-#token       = os.environ.get('SLACK_TOKEN')
 
 app = Flask(__name__)
 
 @app.route('/')
 def health():
+    logger.info('Really, all is ok')
     return "I'm fine"
 
 @app.route('/', methods=['POST'])
 def talent():
 
     request_data = request.form
-    print 'Has: ', slash_token
-    print 'Received: ', request_data['token']
-    
-    if request_data['token'].strip() == slash_token:
+    if valid(request_data):
+        user_id = request_data['user_id']
         text = request_data['text']
-        return 'Did you say ' + text + '?'
+        slack = SlackClient(slack_token)
+        userDataJson = slack.api_call("users.info", user=user_id)
+        user = SlackUser(userDataJson)
+        return 'Did you, ' + user.name + ', say ' + text + '?'
     else:
         return 'Sorry, no'
+
+def valid(request_data):
+    request_token = request_data['token'].strip()
+    if (request_token == slash_token):
+        logger.debug('Token is ok')
+        return True
+    else:
+        logger.warn('Invalid request received!')
+        logger.debug('Has token: ' + slash_token)
+        logger.debug('Received token: ' + request_token)
+        return False
+
 
 if __name__ == "__main__":
     app.run(port=80)
